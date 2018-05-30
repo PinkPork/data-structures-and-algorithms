46774e90642fe927c010150638a35934aa31615f
Jaime Andres Laino Guerra <jaime.laino@JLainoG-2.local>
2018-05-30 10:44:42 -0500

    - create framework

---
+++ Data Structures and Algorithms/Data Structures and Algorithms/Data_Structures_and_Algorithms.h
@@ -0,0 +1,19 @@
+//
+//  Data_Structures_and_Algorithms.h
+//  Data Structures and Algorithms
+//
+//  Created by Jaime Andres Laino Guerra on 5/30/18.
+//  Copyright © 2018 Jaime Andres Laino Guerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Data_Structures_and_Algorithms.
+FOUNDATION_EXPORT double Data_Structures_and_AlgorithmsVersionNumber;
+
+//! Project version string for Data_Structures_and_Algorithms.
+FOUNDATION_EXPORT const unsigned char Data_Structures_and_AlgorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data_Structures_and_Algorithms/PublicHeader.h>
+
+
---
+++ Data Structures and Algorithms/Data Structures and AlgorithmsTests/Data_Structures_and_AlgorithmsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Data_Structures_and_AlgorithmsTests.swift
+//  Data Structures and AlgorithmsTests
+//
+//  Created by Jaime Andres Laino Guerra on 5/30/18.
+//  Copyright © 2018 Jaime Andres Laino Guerra. All rights reserved.
+//
+
+import XCTest
+@testable import Data_Structures_and_Algorithms
+
+class Data_Structures_and_AlgorithmsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
